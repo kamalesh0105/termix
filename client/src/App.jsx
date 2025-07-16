774421c6ff975c415a5e403693492e0c891a5a0e
Jarvis <kamaleshselvam75@gmail.com>
2025-07-17 05:26:23 +0530

    final six

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,6 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar.jsx";
 import DashBoard from "./pages/DashBoard.jsx";
 import Hero from "./pages/Hero.jsx";
-import Termix from "./pages/Termix.jsx";
 const App = () => {
   return (
     <>
@@ -11,7 +10,6 @@ const App = () => {
         <Routes>
           <Route path="/" element={<Hero />}></Route>
           <Route path="/dashboard" element={<DashBoard />}></Route>
-          <Route path="/terminal" element={<Termix />}></Route>
         </Routes>
       </Router>
     </>
